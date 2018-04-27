21636e8801365bccc7c83bfd89b699bc1586cbf9
chang02 <chang0022@snu.ac.kr>
2018-04-27 17:11:51 +0900

    hw3

--- StackCalculator/CalculatorTest.java
+++ StackCalculator/CalculatorTest.java
@@ -1,16 +1,18 @@
 import java.io.*;
 import java.util.Stack;
 import java.util.ArrayList;
+import java.util.regex.*;
 
 public class CalculatorTest
 {
-	boolean error = false;
+	static boolean error = false;
 	public static void main(String args[])
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		while (true)
 		{
+			error = false;
 			try
 			{
 				String input = br.readLine();
@@ -28,17 +30,31 @@ public class CalculatorTest
 
 	private static void command(String input)
 	{
+		String regex = ".*[0-9]+[\t ]+[0-9]+.*";
+		if(input.matches(regex)){
+			error = true;
+		}
 		input = input.replaceAll(" ","");
 		input = input.replaceAll("\t", "");
-		ArrayList<String> parse_input = new ArrayList<>();
-		parse_input = parse(input);
-		String post;
-		post = infixToPostfix(parse_input);
-		
-		System.out.println(post);
-		
-		int result = calculate(post);
-		System.out.println(result);
+		if(input.compareTo("") == 0){
+			System.out.println("ERROR");
+		}
+		else{
+			ArrayList<String> parse_input = parse(input);
+			ArrayList<String> post = infixToPostfix(parse_input);
+			long result = calculate(post);
+			
+			if(error)
+				System.out.println("ERROR");
+			else{
+				String post_result=post.get(0);
+				for(int i=1;i<post.size();i++){
+					post_result = post_result + " " + post.get(i);
+				}
+				System.out.println(post_result);
+				System.out.println(result);
+			}
+		}
 	}
 	
 	static ArrayList<String> parse(String input){
@@ -59,7 +75,7 @@ public class CalculatorTest
 		return parse_input;
 	}
 	
-	static String infixToPostfix(ArrayList<String> infix)
+	static ArrayList<String> infixToPostfix(ArrayList<String> infix)
 	{
 		Stack<String> s = new Stack<>();
 		ArrayList<String> l = new ArrayList<>();
@@ -67,7 +83,8 @@ public class CalculatorTest
 		for(int i=0;i<infix.size();i++) {
 			int c = check(pre, infix.get(i));
 			if(c == -1) {
-				System.out.println("ERROR");
+				error = true;
+				return l;
 			}
 			else if(c == 1) {
 				l.add(infix.get(i));
@@ -77,10 +94,22 @@ public class CalculatorTest
 					l.add(s.pop());
 				}
 				if(s.empty()) {
-					System.out.println("ERROR");
+					error = true;
+					return l;
 				}
 				else {
 					s.pop();
+				}
+			}
+			else if(c == 3){
+				if(s.empty()){
+					s.push(infix.get(i));
+				}
+				else{
+					while(!s.empty() && c > check(" ",s.peek())){
+						l.add(s.pop());
+					}
+					s.push(infix.get(i));
 				}
 			}
 			else if(c == 4) {
@@ -102,7 +131,7 @@ public class CalculatorTest
 					s.push(infix.get(i));
 				}
 				else {
-					while(!s.empty() && c >= check(" ",s.peek())) {
+					while(!s.empty() && c >= check("1",s.peek())) {
 						l.add(s.pop());
 					}
 					s.push(infix.get(i));
@@ -113,15 +142,73 @@ public class CalculatorTest
 		while(!s.empty()) {
 			l.add(s.pop());
 		}
-		String post = ""+l.get(0);
-		for(int i=1;i<l.size();i++) {
-			post = post + " " + l.get(i);
-		}
-		return post;
+		return l;
 	}
-	static int calculate(String post)
+	static long calculate(ArrayList<String> post)
 	{
-		return 0;
+		Stack<Long> s = new Stack<>();
+		for(int i=0;i<post.size();i++){
+			if(isNum(post.get(i))){
+				s.push(Long.parseLong(post.get(i)));
+			}
+			else if(post.get(i).compareTo("~") == 0){
+				if(s.empty()){
+					error = true;
+				}
+				else{
+					long tmp = s.pop();
+					tmp = tmp * (-1);
+					s.push(tmp);
+				}
+			}
+			else{
+				if(s.size() < 2){
+					error = true;
+				}
+				else{
+					long tmp1 = s.pop();
+					long tmp2 = s.pop();
+					if(post.get(i).compareTo("+") == 0){
+						s.push(tmp2+tmp1);
+					}
+					else if(post.get(i).compareTo("-") == 0){
+						s.push(tmp2-tmp1);
+					}
+					else if(post.get(i).compareTo("*") == 0){
+						s.push(tmp2*tmp1);
+					}
+					else if(post.get(i).compareTo("/") == 0){
+						if(tmp1 == 0){
+							error = true;
+							return 0;
+						}
+						s.push(tmp2/tmp1);
+					}
+					else if(post.get(i).compareTo("%") == 0){
+						if(tmp1 == 0){
+							error = true;
+							return 0;
+						}
+						s.push(tmp2%tmp1);
+					}
+					else if(post.get(i).compareTo("^") == 0){
+						s.push((long)Math.pow(tmp2, tmp1));
+					}
+				}
+			}
+		}
+		if(s.empty()){
+			error = true;
+			return 0;
+		}
+		long result = s.pop();
+		if(s.empty()){
+			return result;
+		}
+		else{
+			error = true;
+			return 0;
+		}
 	}
 	static int check(String pre, String now) {
 		if(isNum(now)) {
